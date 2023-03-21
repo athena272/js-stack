1258d6daac466f8336bebb17a3990af6298f953f
athena272 <guilhermera272@gmail.com>
2023-03-21 14:19:07 -0300

    Update 06-destructuring.js

--- 00-js-moderno/01-default-parameters/06-destructuring.js
+++ 00-js-moderno/01-default-parameters/06-destructuring.js
@@ -1,7 +1,16 @@
 const user = {
     firstName: 'Mateus',
     lastName: 'Mateus',
+    full_name: 'Mateus Silva',
     age: 23,
-    instagram: '@imateus.silva'
-    
+    instagram: '@imateus.silva',
+    skills: ['back-end', 'front-end', 'mobile', 'ui/ux']
 }
+
+//Destructuring
+// const firstName = user.firstName
+const { firstName, skills, full_name: fullName } = user
+const [back, front, mobile, design] = skills
+
+console.log(fullName)
+console.log(`${back}\n${front}\n${mobile}\n${design}`)
