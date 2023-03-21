eb99b589eaff348af0d5d83f746d3e64e3fea204
athena272 <guilhermera272@gmail.com>
2023-03-21 20:20:03 -0300

    Create 08-spred-operator.js

---
+++ 00-js-moderno/08-spred-operator.js
@@ -0,0 +1,19 @@
+const user = {
+    firstName: 'Mateus',
+    lastName: 'Mateus',
+    full_name: 'Mateus Silva',
+    age: 23,
+    instagram: '@imateus.silva',
+    skills: ['back-end', 'front-end', 'mobile', 'ui/ux'],
+    active: false,
+}
+
+//Spread (espalhar) operator
+const updateUser = {
+    ...user,
+    active: true,
+
+}
+
+console.log(user)
+console.log(updateUser)
