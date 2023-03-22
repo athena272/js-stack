49e9a7363d926a024f27ad250ce3cf8a71616837
athena272 <guilhermera272@gmail.com>
2023-03-22 16:03:39 -0300

    Update 00-regex-maria.js

--- 00-js-moderno/00-regex-maria.js
+++ 00-js-moderno/00-regex-maria.js
@@ -1,6 +1,7 @@
-const regex = /^[a-z]$/
-const str = 'AAAAAAAAAaaa';
+// const regex = /^[a-z]$/
+const str = '(87) 55455-7433';
 
+/^\(\d{2}\) (?!.*([0-9\-])\1{6,})[0-9\-]{1,}$/
 if (regex.test(str)) {
     console.log('Documento de identificação válido!');
 } else {
