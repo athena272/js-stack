cfe6e4df3aedadd1cf0c024d3167d787582b6ce9
athena272 <guilhermera272@gmail.com>
2023-03-23 02:15:56 -0300

    Delete 11-includes.js

--- 00-js-moderno/11-includes.js
+++
@@ -1,8 +0,0 @@
-const tech = 'Node.js'
-const array = ['Node.js', 'React', 'Typescript']
-
-const includesString = tech.includes('node')
-const includesArray = array.includes('React')
-
-console.log({ includesString })
-console.log({ includesArray })
