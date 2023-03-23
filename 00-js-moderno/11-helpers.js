8e0891b5560063887bfd5e75a595a2d3562a77f6
athena272 <guilhermera272@gmail.com>
2023-03-23 14:39:45 -0300

    Update 11-helpers.js

--- 00-js-moderno/11-helpers.js
+++ 00-js-moderno/11-helpers.js
@@ -3,6 +3,11 @@ const array = ['Node.js', 'React', 'Typescript']
 
 const includesString = tech.includes('node')
 const includesArray = array.includes('React')
-
 console.log({ includesString })
 console.log({ includesArray })
+
+const startsWith = tech.startsWith('node')
+const endsWith = tech.endsWith('.js')
+
+console.log({ startsWith })
+console.log({ endsWith })
