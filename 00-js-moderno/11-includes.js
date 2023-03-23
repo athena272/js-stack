80eb1c93b75f4619fd5bc683799b68a8d01d104f
athena272 <guilhermera272@gmail.com>
2023-03-23 02:15:35 -0300

    Create 11-includes.js

---
+++ 00-js-moderno/11-includes.js
@@ -0,0 +1,8 @@
+const tech = 'Node.js'
+const array = ['Node.js', 'React', 'Typescript']
+
+const includesString = tech.includes('node')
+const includesArray = array.includes('React')
+
+console.log({ includesString })
+console.log({ includesArray })
