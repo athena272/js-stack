d32dc2a4b0f3c7e34f9d1a35caef7cc809e8c74d
athena272 <guilhermera272@gmail.com>
2023-03-28 01:37:12 -0300

    Create index.js

---
+++ 01-fundamentos-react/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _react = require("react");
+var _reactDom = require("react-dom");
+// const user = {}
+
+// // Optional chaining
+// console.log(user?.address?.stree)
+
+_reactDom.ReactDOM.render( /*#__PURE__*/_react.React.createElement("h1", {
+  id: "title"
+}, "Hello world!!!"), document.getElementById('root'));
