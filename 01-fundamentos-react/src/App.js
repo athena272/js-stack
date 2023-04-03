d7156efbd82499dde78e3fe6540bc1d790b3b44f
athena272 <guilhermera272@gmail.com>
2023-04-02 21:18:46 -0300

    Update App.js

--- 01-fundamentos-react/src/App.js
+++ 01-fundamentos-react/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react'
+import { Post } from './Post.js'
 
 
 const postOfWeek = 'Posts da semana'
 
 // Render -> Renderizar
+// Props
 export function App() {
     return (
         <React.Fragment>
             <h1>JStack Blogs</h1>
             <h2>{postOfWeek}</h2>
+
+            <hr />
+
+            <Post
+                post={{
+                    title: 'Titulo Noticia 01',
+                    subtitle: 'Subtitule Noticia 01'
+                }}
+            />
+            <Post
+                post={{
+                    title: 'Título Noticia 02',
+                    subtitle: 'Sutítulo Noticia 02'
+                }}
+            />
+            <Post
+                post={{
+                    title: 'Título Noticia 03',
+                    subtitle: 'Sutítulo Noticia 03'
+                }}
+            />
+            <Post
+                post={{
+                    title: 'Título Noticia 04',
+                    subtitle: 'Sutítulo Noticia 04'
+                }}
+            />
         </React.Fragment>
     )
 } 
