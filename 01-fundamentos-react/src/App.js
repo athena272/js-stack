c4f6cd90ec2f2c2cc58150362386926b8a462c9f
athena272 <guilhermera272@gmail.com>
2023-04-03 15:56:18 -0300

    Update App.js

--- 01-fundamentos-react/src/App.js
+++ 01-fundamentos-react/src/App.js
@@ -10,25 +10,28 @@ const postOfWeek = 'Posts da semana'
 export function App() {
     return (
         <React.Fragment>
-            <Header title='JStack Pro Blog'>
+            <Header>
                 <h2>{postOfWeek}</h2>
             </Header>
 
             <hr />
 
             <Post
+                likes={220}
                 post={{
                     title: 'Titulo Noticia 01',
                     subtitle: 'Subtitule Noticia 01'
                 }}
             />
             <Post
+                likes={530}
                 post={{
                     title: 'Título Noticia 02',
                     subtitle: 'Sutítulo Noticia 02'
                 }}
             />
             <Post
+                likes={470}
                 post={{
                     title: 'Título Noticia 03',
                     subtitle: 'Sutítulo Noticia 03'
