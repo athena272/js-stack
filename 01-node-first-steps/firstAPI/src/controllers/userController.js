01dd2d57504640f61be3ad28bc44f709a22e9022
athena272 <guilhermera272@gmail.com>
2023-04-07 13:50:34 -0300

    Update userController.js

--- 01-node-first-steps/firstAPI/src/controllers/userController.js
+++ 01-node-first-steps/firstAPI/src/controllers/userController.js
@@ -1,10 +1,19 @@
 const users = require('../mocks/users.js')
 
 function listUsers(req, res) {
+    const { order } = req.query
+    const usersSorted = users.sort((a, b) => {
+        if (order === 'desc') {
+            return a.id < b.id ? 1 : -1
+        }
+
+        return a.id > b.id ? 1 : -1
+    })
+
     res.writeHead(200, {
         'Content-Type': 'application/json'
     })
-    res.end(JSON.stringify(users)) 
+    res.end(JSON.stringify(users))
 }
 
 module.exports = { listUsers }
