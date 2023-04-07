cc19df440830ce2cf35d0b5187a293f4419632c6
athena272 <guilhermera272@gmail.com>
2023-04-07 18:58:11 -0300

    Update userController.js

--- 01-node-first-steps/firstAPI/src/controllers/userController.js
+++ 01-node-first-steps/firstAPI/src/controllers/userController.js
@@ -16,4 +16,23 @@ function listUsers(req, res) {
     res.end(JSON.stringify(usersSorted))
 }
 
-module.exports = { listUsers }
+function getUserById(req, res) {
+    const { id } = req.params
+    const user = users.find((user) => user.id === Number(id))
+
+    if (!user) {
+        res.writeHead(400, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify({ error: 'User not found' }))
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify(user))
+    }
+
+
+}
+
+module.exports = { listUsers, getUserById }
