1d4db9d4915160b8491a0eaa075314af34bc6ebe
athena272 <guilhermera272@gmail.com>
2023-04-07 11:56:20 -0300

    Create users.js

---
+++ 01-node-first-steps/firstAPI/src/mocks/users.js
@@ -0,0 +1,20 @@
+users = [
+    {
+        id: 1,
+        name: 'John',
+    },
+    {
+        id: 2,
+        name: 'Deborah'
+    },
+    {
+        id: 3,
+        name: 'Victor'
+    },
+    {
+        id: 4,
+        name: 'Pedra Verde'
+    }
+]
+
+module.exports = users
