467c37b875e88030aea7d8964c49788bb8b41155
athena272 <guilhermera272@gmail.com>
2023-04-07 12:07:21 -0300

    Update index.js

--- 01-node-first-steps/firstAPI/src/index.js
+++ 01-node-first-steps/firstAPI/src/index.js
@@ -1,10 +1,25 @@
 const http = require('http')
+const users = require('./mocks/users.js')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    res.end('<h1>Welcome to live!</h1>')
+    console.log(`Method: ${req.method} | Endpoint: ${req.url}`)
+
+    if (req.url === '/users' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify(users))
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        })
+        res.end(`Cannot ${req.method} ${req.url}`)
+    }
+
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // })
+    // res.end('<h1>Welcome to live!</h1>')
 })
 
 server.listen(3000, () => console.log('🔥 server on fire 🔥'))
