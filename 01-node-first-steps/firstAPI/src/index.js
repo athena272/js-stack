d323d3498419f54515c1d07933cbd9506d2bcd54
athena272 <guilhermera272@gmail.com>
2023-04-08 17:23:32 -0300

    Update index.js

--- 01-node-first-steps/firstAPI/src/index.js
+++ 01-node-first-steps/firstAPI/src/index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const url = require('url')
 const routes = require('./routes')
+const { bodyParser } = require('./helpers/bodyParser.js')
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true)
@@ -29,8 +30,13 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(body))
         }
 
-        route.handler(req, res)
+        if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+            bodyParser(req, () => route.handler(req, res))
 
+        } else {
+            route.handler(req, res)
+
+        }
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html'
