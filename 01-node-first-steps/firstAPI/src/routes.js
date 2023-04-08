922d0bda137f5ffd132943b6cb6e7cb6bde1b034
athena272 <guilhermera272@gmail.com>
2023-04-08 17:08:24 -0300

    Update routes.js

--- 01-node-first-steps/firstAPI/src/routes.js
+++ 01-node-first-steps/firstAPI/src/routes.js
@@ -1,4 +1,4 @@
-const { listUsers, getUserById } = require('./controllers/userController.js')
+const { listUsers, getUserById, createUser } = require('./controllers/userController.js')
 
 module.exports = [
     {
@@ -10,5 +10,10 @@ module.exports = [
         endpoint: '/users/:id',
         method: 'GET',
         handler: getUserById
-    }
+    },
+    {
+        endpoint: '/users',
+        method: 'POST',
+        handler: createUser
+    },
 ]
