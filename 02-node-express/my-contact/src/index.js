96868410568fbc201f3ac72b9b3b305c322100f4
athena272 <guilhermera272@gmail.com>
2023-04-15 01:21:25 -0300

    Update index.js

--- 02-node-express/my-contact/src/index.js
+++ 02-node-express/my-contact/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my API')
+    res.send('🔥 Welcome to my API 🔥')
 })
 
 app.listen(3000, () => console.log('🔥 Server on fire 🔥 at http://localhost:3000'))
