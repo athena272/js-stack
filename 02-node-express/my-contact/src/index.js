3e8b6c41088c956bb99a905652fedbc7590e74b0
athena272 <guilhermera272@gmail.com>
2023-04-15 01:12:58 -0300

    Create index.js

---
+++ 02-node-express/my-contact/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API')
+})
+
+app.listen(3000, () => console.log('🔥 Server on fire 🔥 at http://localhost:3000'))
