b1885a18bef73aedfdff34dbc8102301a78c32d6
athena272 <guilhermera272@gmail.com>
2023-04-16 15:52:44 -0300

    Update ContactController.js

--- 02-node-express/my-contact/src/app/controllers/ContactController.js
+++ 02-node-express/my-contact/src/app/controllers/ContactController.js
@@ -33,7 +33,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email)
 
     if (contactExists) {
-      return res.status(400).json({ errorMessage: 'This e-mail is already been taken' })
+      return res.status(400).json({ errorMessage: 'This e-mail is already in use' })
     }
 
     const contact = await ContactsRepository.create({
@@ -43,8 +43,30 @@ class ContactController {
     res.json(contact)
   }
 
-  update() {
+  async update(req, res) {
     // Update the contact
+    const { id } = req.params
+    const { name, email, phone, category_id } = req.body
+
+    const contactExists = await ContactsRepository.findById(id)
+    if (!contactExists) {
+      return res.status(404).json({ errorMessage: 'Contact not found' })
+    }
+
+    if (!name) {
+      return res.status(400).json({ errorMessage: 'Name is required' })
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email)
+    if (contactByEmail && contactByEmail.id !== id) {
+      return res.status(400).json({ errorMessage: 'This e-mail is already in use' })
+    }
+
+    const contact = await ContactsRepository.updateContact(id, {
+      name, email, phone, category_id
+    })
+
+    res.json(contact)
   }
 
   async delete(req, res) {
