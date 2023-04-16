9feb85da7114eb65c8f412aa0c4001e7e27303d2
athena272 <guilhermera272@gmail.com>
2023-04-16 14:47:07 -0300

    Update routes.js

--- 02-node-express/my-contact/src/routes.js
+++ 02-node-express/my-contact/src/routes.js
@@ -1,11 +1,19 @@
-const { Router } = require('express')
+const { Router, request } = require('express')
 const ContactController = require('./app/controllers/ContactController')
 // const { index } = require('./app/controllers/ContactController')
 
 // const route = express.Router()
 const router = Router()
 
-router.get('/contacts', ContactController.index)
+router.get(
+  '/contacts',
+  (req, res, next) => {
+    req.appId = 'MyID'
+    next()
+  },
+
+  ContactController.index,
+)
 router.get('/contacts/:id', ContactController.show)
 router.delete('/contacts/:id', ContactController.delete)
 
