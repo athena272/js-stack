c74e4d784913706ed4816899c5108611f918bbe9
athena272 <guilhermera272@gmail.com>
2023-04-21 22:11:54 -0300

    Delete schema.sql

--- 02-node-express/my-contact/src/database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE mycontacts;
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS categories (
-  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-  name VARCHAR NOT NULL
-);
