6ba424a4a11356d558b8fb11570897eb1e3aca69
athena272 <guilhermera272@gmail.com>
2023-04-21 22:12:18 -0300

    Create routes.js

---
+++ 02-node-express/my-contacts/src/routes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+const ContactController = require('./app/controllers/ContactController')
+// const { index } = require('./app/controllers/ContactController')
+
+// const route = express.Router()
+const router = Router()
+
+router.get('/contacts', ContactController.index)
+router.get('/contacts/:id', ContactController.show)
+router.delete('/contacts/:id', ContactController.delete)
+router.post('/contacts', ContactController.store)
+router.put('/contacts/:id', ContactController.update)
+
+module.exports = router
