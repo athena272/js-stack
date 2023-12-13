f30e24f1f6a95c1d321a27dbcb539e55f6614298
athena272 <guilhermera272@gmail.com>
2023-12-13 08:12:24 -0300

    Create index.js

---
+++ 03-fundamentos-react/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _user$address;
+var user = {};
+console.log(user === null || user === void 0 || (_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
