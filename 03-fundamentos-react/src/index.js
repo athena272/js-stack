3a6252843c7b5ff81e714272e72fb052455ab627
athena272 <guilhermera272@gmail.com>
2023-12-14 22:04:19 -0300

    Update index.js

--- 03-fundamentos-react/src/index.js
+++ 03-fundamentos-react/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-    
+    <>
+        <h1>Hoya World!!!</h1>
+        document.getElementById("root")
+    </>
 )
