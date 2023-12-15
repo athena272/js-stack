b96499f8579c74357f2950f6609452b931d8ae5e
athena272 <guilhermera272@gmail.com>
2023-12-14 23:17:09 -0300

    Delete index.js

--- 03-fundamentos-react/build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _user$address;
-var user = {};
-console.log(user === null || user === void 0 || (_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
