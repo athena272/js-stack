6e20079061990a10ecd179bf0933e1ea37d1acc9
athena272 <guilhermera272@gmail.com>
2023-12-15 22:21:19 -0300

    Create App.jsx

---
+++ 03-fundamentos-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./components/Post.jsx";
+
+const App = () => (
+    <>
+        <h1>
+            🔥🔥🔥 JS Blog 🔥🔥🔥
+        </h1>
+        <h2>Post da semana</h2>
+        <hr />
+       
+        <Post />
+        <Post />
+        <Post />
+    </>
+)
+
+export default App
