8aa66ec7f637aa077ed560263f4378ea2c5545be
athena272 <guilhermera272@gmail.com>
2023-12-16 11:14:11 -0300

    Update Post.jsx

--- 03-fundamentos-react/src/components/Post.jsx
+++ 03-fundamentos-react/src/components/Post.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types"
 
-const Post = () => (
+const Post = ({ post, likes }) => (
     <>
         <article>
-            <strong>Título do post</strong> <br />
-            <small>Sub-título do post</small>
+            <strong>{post.title}</strong> <br />
+            <small>{post.subtitle}</small> <br />
+
+            Likes: {likes}
         </article>
 
         <br />
     </>
 )
+
+Post.propTypes = {
+    likes: PropTypes.number.isRequired,
+    post: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+    }
+    ).isRequired
+}
 
 export default Post
