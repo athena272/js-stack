86013c3073d5d503e06be69f1c7382c2ca7aa133
athena272 <guilhermera272@gmail.com>
2023-12-16 11:14:09 -0300

    Create index.js

---
+++ 03-fundamentos-react/src/data/index.js
@@ -0,0 +1,22 @@
+export const posts = [
+    {
+        title: "Título número 1",
+        subtitle: "Subtítulo número 1",
+        likes: 100
+    },
+    {
+        title: "Título número 2",
+        subtitle: "Subtítulo número 2",
+        likes: 200
+    },
+    {
+        title: "Título número 3",
+        subtitle: "Subtítulo número 3",
+        likes: 300
+    },
+    {
+        title: "Título número 4",
+        subtitle: "Subtítulo número 4",
+        likes: 400
+    },
+]
