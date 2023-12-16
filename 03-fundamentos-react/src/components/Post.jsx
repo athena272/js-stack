82659941be4eece81c0ff4a3f358ae98c536da97
athena272 <guilhermera272@gmail.com>
2023-12-15 22:21:17 -0300

    Create Post.jsx

---
+++ 03-fundamentos-react/src/components/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Post = () => (
+    <>
+        <article>
+            <strong>Título do post</strong> <br />
+            <small>Sub-título do post</small>
+        </article>
+
+        <br />
+    </>
+)
+
+export default Post
