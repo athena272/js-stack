5a1069c4afa7037b7c3a6ccf8cc2771c8a2390f0
athena272 <guilhermera272@gmail.com>
2023-12-17 00:05:35 -0300

    Update Button.jsx

--- 03-fundamentos-react/src/components/Button/Button.jsx
+++ 03-fundamentos-react/src/components/Button/Button.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from "prop-types"
 
-const Button = ({ onClick, children }) => (
-    <button onClick={onClick}>
+const Button = ({ onClick, children, theme }) => (
+
+    <button
+        onClick={onClick}
+        style={{
+            marginRight: 8,
+            marginLeft: 8,
+            backgroundColor: theme === 'light' ? '#fff' : '#000',
+            color: theme === 'light' ? '#000' : '#fff'
+        }}
+    >
         {children}
     </button>
 )
 
 Button.propTypes = {
+    theme: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
     children: PropTypes.node.isRequired
 }
