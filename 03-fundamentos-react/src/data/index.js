d247a7b6df9152fc9498731e68acf166852e7654
athena272 <guilhermera272@gmail.com>
2023-12-16 23:02:30 -0300

    Update index.js

--- 03-fundamentos-react/src/data/index.js
+++ 03-fundamentos-react/src/data/index.js
@@ -1,22 +1,30 @@
 export const posts = [
     {
+        id: Math.random(),
         title: "Título número 1",
         subtitle: "Subtítulo número 1",
-        likes: 100
+        likes: 100,
+        isRead: false,
     },
     {
+        id: Math.random(),
         title: "Título número 2",
         subtitle: "Subtítulo número 2",
-        likes: 200
+        likes: 200,
+        isRead: true,
     },
     {
+        id: Math.random(),
         title: "Título número 3",
         subtitle: "Subtítulo número 3",
-        likes: 300
+        likes: 300,
+        isRead: false,
     },
     {
+        id: Math.random(),
         title: "Título número 4",
         subtitle: "Subtítulo número 4",
-        likes: 400
+        likes: 400,
+        isRead: false,
     },
 ]
