03606e814e717d41dc30f38abc0e3336439912ce
athena272 <guilhermera272@gmail.com>
2023-12-16 23:02:50 -0300

    Update App.jsx

--- 03-fundamentos-react/src/App.jsx
+++ 03-fundamentos-react/src/App.jsx
@@ -1,27 +1,63 @@
-import React from "react";
-import Header from "./components/Header.jsx";
+import React, { useState } from "react";
+import Header from "./components/Header/Header.jsx";
 import { posts } from "./data/index.js";
-import Post from "./components/Post.jsx";
+import Post from "./components/Post/Post.jsx";
 
-const App = () => (
-    <>
-        <Header>
-            <h2>Posts da semana</h2>
-        </Header>
+const App = () => {
+    const [theme, setTheme] = useState('dark')
+    const [postsToUse, setPostsToUse] = useState(posts)
 
-        <hr />
+    const handleToggleTheme = () => {
+        setTheme(currentTheme => currentTheme === 'dark' ? 'light' : 'dark')
+    }
 
-        {posts.map((post, index) => (
-            <Post
-                key={index}
-                likes={post.likes}
-                post={{
-                    title: post.title,
-                    subtitle: post.subtitle
-                }}
-            />
-        ))}
-    </>
-)
+    const handleRefresh = () => {
+        const postNumber = postsToUse.length + 1
+        const newPost = {
+            id: Math.random(),
+            title: `Título número ${postNumber}`,
+            subtitle: `Subtítulo número ${postNumber}`,
+            likes: postNumber * 100
+        }
+
+        setPostsToUse(currentPosts => [...currentPosts, newPost])
+        // setTimeout(() => {
+        //     setPostsToUse((currentPosts) => {
+        //         return [...currentPosts,
+        //         {
+        //             title: `Título número ${currentPosts.length + 1}`,
+        //             subtitle: `Subtítulo número ${currentPosts.length + 1}`,
+        //             likes: (currentPosts.length + 1) * 100
+        //         }]
+        //     })
+        // }, 2 * 1000)
+
+        console.log(postsToUse)
+        console.log("Refreshing")
+    }
+
+    const handleRemove = (postId) => {
+        setPostsToUse(currentPost => currentPost.filter(post => post.id !== postId))
+    }
+
+    return (
+        <>
+            <Header onToggleTheme={handleToggleTheme}>
+                <h2>Posts da semana</h2>
+                <button onClick={handleRefresh}>Render again</button>
+            </Header>
+
+            <hr />
+
+            {postsToUse.map((post, index) => (
+                <Post
+                    key={index}
+                    post={post}
+                    onRemove={handleRemove}
+                />
+            ))}
+        </>
+    )
+}
 
 export default App
