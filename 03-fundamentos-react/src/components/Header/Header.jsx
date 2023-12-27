b3e43773895f21826a153d656b130ec5b0b764c0
athena272 <guilhermera272@gmail.com>
2023-12-27 00:21:42 -0300

    Update Header.jsx

--- 03-fundamentos-react/src/components/Header/Header.jsx
+++ 03-fundamentos-react/src/components/Header/Header.jsx
@@ -1,22 +1,31 @@
 import React, { useContext } from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 import Button from "../Button/Button.jsx"
 import { ThemeContext } from "../../context/ThemeContext/ThemeContext.jsx"
-import styles from './Header.scss'
+
+const Title = styled.h1`
+    color: #637BF3;
+`
+
+const Div = styled.div`
+    background-color: #393939;
+
+`
 
 const Header = ({ title, children }) => {
     const { onToggleTheme } = useContext(ThemeContext)
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>
+        <Div>
+            <Title>
                 {title}
-            </h1>
+            </Title>
             {children}
             <Button onClick={onToggleTheme}>
                 Change Theme
             </Button>
-        </div>
+        </Div>
     )
 }
 
