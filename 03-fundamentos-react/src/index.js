5885ba139e36158f77fa9de1d3ae389fbd48b0e6
athena272 <guilhermera272@gmail.com>
2023-12-27 00:21:18 -0300

    Update index.js

--- 03-fundamentos-react/src/index.js
+++ 03-fundamentos-react/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App.jsx'
+import GlobalStyle from "./styles/global.js";
 
 ReactDOM.render(
-    <App />,
+    <>
+        <GlobalStyle />
+        <App />
+    </>
+    ,
     document.getElementById("root")
 )
