008dc31c103b958a803119e146049996a9b17294
athena272 <guilhermera272@gmail.com>
2024-01-09 05:56:10 -0300

    Create styles.js

---
+++ 03-fundamentos-react/src/components/Title/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+    color: #637BF3;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+`
+
+export const Div = styled.div`
+    background-color: #393939;
+`
