f8a412427037a4b2f74398e0a21034248ec10c39
athena272 <guilhermera272@gmail.com>
2024-01-09 09:40:05 -0300

    Update styles.js

--- 03-fundamentos-react/src/components/Post/styles.js
+++ 03-fundamentos-react/src/components/Post/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SubTitle = styled.small`
 border: 1px solid red;
@@ -6,4 +6,15 @@ border: 1px solid red;
 
 export const Rate = styled.span`
 border: 1px solid blue;
+`
+
+export const Container = styled.article`
+    margin-bottom: 24px;
+    /* opacity: ${(props) => props.isRemove ? 0.5 : 1};
+    color: ${(props) => props.isRemove ? '#f00' : '#fff'}; */
+
+    ${(props) => css`
+        opacity: ${props.isRemove ? 0.5 : 1};
+        color: ${props.isRemove ? '#f00' : '#fff'};
+    `}
 `
