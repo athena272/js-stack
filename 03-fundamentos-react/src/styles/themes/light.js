a0e468b9265e8f462047ffa707680ad4e2da8d83
athena272 <guilhermera272@gmail.com>
2024-01-10 15:04:37 -0300

    Update light.js

--- 03-fundamentos-react/src/styles/themes/light.js
+++ 03-fundamentos-react/src/styles/themes/light.js
@@ -4,5 +4,8 @@ export default {
   ...commonStyles,
   backgroundColor: '#fff',
   textColor: '#222',
+  headerBackgroundColor: '#ddd',
+  footerBackgroundColor: '#ddd',
+  postBackgroundColor: '#ddd',
 };
 
