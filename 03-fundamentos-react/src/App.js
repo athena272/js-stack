b05f109370dd73f4e507128cfdc4620f80191efa
athena272 <guilhermera272@gmail.com>
2024-01-13 06:22:59 -0300

    Update App.js

--- 03-fundamentos-react/src/App.js
+++ 03-fundamentos-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -7,10 +7,23 @@ import Layout from './components/Layout';
 import themes from './styles/themes'
 
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark
+  }, [theme])
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark')
+  }
+
   return (
-    <ThemeProvider theme={themes.light}>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout />
+      <Layout
+        onToggleTheme={handleToggleTheme}
+        selectedTheme={theme}
+      />
     </ThemeProvider>
   );
 };
