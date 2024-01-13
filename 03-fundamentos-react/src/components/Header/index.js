3f453df3a08340a65529b66d42d84694004a1501
athena272 <guilhermera272@gmail.com>
2024-01-13 06:23:17 -0300

    Update index.js

--- 03-fundamentos-react/src/components/Header/index.js
+++ 03-fundamentos-react/src/components/Header/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header() {
+export default function Header({ onToggleTheme, selectedTheme }) {
   return (
     <Container>
       <h1>JStack's Blog</h1>
-      <button type="button">🌞</button>
+      <button type="button" onClick={onToggleTheme}>
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+      </button>
     </Container>
   );
 }
