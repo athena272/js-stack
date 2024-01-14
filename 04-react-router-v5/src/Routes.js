e1efbffbf35c6822a88f547db1d99665d9b7cae2
athena272 <guilhermera272@gmail.com>
2024-01-14 16:10:56 -0300

    Update Routes.js

--- 04-react-router-v5/src/Routes.js
+++ 04-react-router-v5/src/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom"
 import Home from "./Pages/Home";
 import Posts from "./Pages/Posts";
+import Post from "./Pages/Posts/Post";
+import NotFound from "./Pages/NotFound";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/posts" component={Posts} />
+            <Route exact path="/posts" component={Posts} />
+            <Route path="/posts/:id" component={Post} />
+            <Route path="*" component={NotFound} />
         </Switch>
     )
 }
