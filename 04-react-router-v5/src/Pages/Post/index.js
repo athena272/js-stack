d90b4052cd95b8c3e89b4e8c27efb32f863a6563
athena272 <guilhermera272@gmail.com>
2024-01-16 00:08:36 -0300

    Update index.js

--- 04-react-router-v5/src/Pages/Post/index.js
+++ 04-react-router-v5/src/Pages/Post/index.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Home() {
+    const params = useParams()
+    
+    console.log(params)
+
     return (
         <h1>Post page</h1>
     )
