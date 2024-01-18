a009e4bc2aa1da7fe854a6eb8ccd98b89f4b6daf
athena272 <guilhermera272@gmail.com>
2024-01-18 01:37:52 -0300

    Update index.js

--- 04-react-router-v5/src/Pages/Post/index.js
+++ 04-react-router-v5/src/Pages/Post/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useMemo } from "react";
+import { useParams, useLocation } from "react-router-dom";
 
 export default function Home() {
     const params = useParams()
-    
+    const { search } = useLocation()
+
+    const queryParams = useMemo(() => new URLSearchParams(search), [search])
+
+
     console.log(params)
+    console.log(queryParams.get('meuTeste'))
 
     return (
         <h1>Post page</h1>
