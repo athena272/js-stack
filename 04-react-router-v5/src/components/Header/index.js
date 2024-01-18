b2b4de940151cdb413764d2b07ed5b7b1beb3123
athena272 <guilhermera272@gmail.com>
2024-01-18 03:32:13 -0300

    Update index.js

--- 04-react-router-v5/src/components/Header/index.js
+++ 04-react-router-v5/src/components/Header/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 
 export default function Header() {
+  const history = useHistory()
+
+  function handleNavigate() {
+    history.push('/posts')
+  }
+
   return (
     <Container>
       <h1>JStack's Blog</h1>
       <button type="button">🌞</button>
+      <button onClick={handleNavigate} style={{ color: " #fff" }}>Navegar</button>
     </Container>
   );
 }
